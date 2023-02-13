0fd22e1ab74cccb367256f6313b87df0881694dd
Esteban Pelutiero <estebanpelutiero@gmail.com>
2023-02-13 15:09:19 -0300

    agregado de botones y links para navegacion, sidebar finalizada

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import "./sidebar.css";
+import SidebarButton from '../sidebarButton/SidebarButton';
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay, FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar-container'>Sidebar</div>
+    <div className='sidebar-container'>
+      <img src="https://res.cloudinary.com/dn6w1lstv/image/upload/v1676303582/music-player-react/icon_jmly43.png"
+      className='profile-img' 
+      alt="profile" />
+
+      <div>
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />}/>
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />}/>
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />}/>
+        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />}/>
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />}/>
+      </div>
+
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />}/>
+    </div>
   )
 }
 
